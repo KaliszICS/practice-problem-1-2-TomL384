2b96bba38262a24b2c267cf45990a6e368f66ccb
TomL384 <350286454@tdsb.ca>
2026-02-12 16:05:05 +0000

    Final.Tom Leung

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -7,25 +7,56 @@ public class PracticeProblem {
 		q4();
 		q5();
 	}
-
+ 
 	public static void q1() {
 		//Write question 1 code here
+		int a ;
+		int b ;
+		int c ;
+		a= 2 ; 
+		b= 5 ; 
+		c= 3 ;
+		System.out.println(a+b+c) ;
+		System.out.println(a*b*c) ;
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int a ;
+		int b ;
+		a = 10 ;
+		b = 3 ;
+		System.out.println(a/b);
+		System.out.println(a % b);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		double a ;
+		double b ;
+		a = 5.3 ;
+		b = 5.2 ;
+		System.out.println(a/b);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int a ;
+		int b ;
+		int c ;
+		a= 2 ; 
+		b= 3 ; 
+		c= 4 ;
+		System.out.println((a+b)*c) ;
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		int a ;
+		double b ;
+		a = 5 ;
+		b = 3.5 ;
+		System.out.println(a+b);
 	}
 
 }
